c813066ef87a016f9d95fab43d0a057a48fdeac5
Michela Di Simone <disimonemichela@gmail.com>
2024-02-01 17:19:53 +0100

    aggiunte la classe di holdout e di k fold cross validation

---
+++ holdout.py
@@ -0,0 +1,61 @@
+import numpy as np
+class Holdout:
+    """
+    Modella la tecnica di holdout per la suddivisione di un dataset in training set e test set.
+    """
+    def __init__(self, data, target, train_size=0.8, random_state=0):
+        """
+        Costruttore
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            il dataset da suddividere
+        target : pandas.Series
+            la serie di valori target
+        train_size : float, optional
+            la proporzione di esempi da assegnare al training set. Di default è 0.8.
+        random_state : int, optional
+            il seme per la generazione di numeri casuali. Di default è 0.
+
+        Returns
+            ----
+        None
+        """
+        self.data = data
+        self.target = target
+        self.train_size = train_size
+        self.random_state = random_state
+        self.train = None
+        self.test = None
+        self.train_target = None
+        self.test_target = None
+        self.split()
+    def split(self):
+        """
+        Suddivide il dataset in training set e test set.
+
+        Returns
+        -------
+        pandas.DataFrame
+            il training set
+        pandas.DataFrame
+            il test set
+        pandas.Series
+            i valori target del training set
+        pandas.Series
+            i valori target del test set
+
+        """
+        #assicura che la sequenza di numeri casuali generata sia sempre la stessa per lo stesso valore di random_state
+        np.random.seed(self.random_state)
+        train_index = np.random.choice(self.data.index, size=int(self.train_size*len(self.data)), replace=False)
+        test_index = self.data.index[~self.data.index.isin(train_index)] #  ~ è l'operatore di negazione logica: prende gli indici che non sono in train_index
+        #generazione dei train set e test set
+        self.train = self.data.loc[train_index] #loc seleziona le righe con gli indici indicati
+        self.test = self.data.loc[test_index]
+        #generazione dei target
+        self.train_target = self.target.loc[train_index]
+        self.test_target = self.target.loc[test_index]
+        return self.train, self.test, self.train_target, self.test_target
+
---
+++ kfoldcrossvalidation.py
@@ -0,0 +1,61 @@
+import numpy as np
+class KFoldCrossValidation:
+    """
+    Modella la tecnica di k-fold cross validation per la suddivisione di un dataset in training set e test set.
+    """
+    def __init__(self, data, target, K=10, random_state=0):
+        """
+        Costruttore
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            il dataset da suddividere
+        target : pandas.Series
+            la serie di valori target
+        K : int
+            il numero di fold. Di default è 10.
+        random_state : int, optional
+            il seme per la generazione di numeri casuali. Di default è 0.
+
+        Returns
+            ----
+        None
+        """
+        self.data = data
+        self.target = target
+        self.K = K
+        self.random_state = random_state
+        self.fold = []
+        self.split()
+    def split(self):
+        """
+        Suddivide il dataset in training set e test set.
+
+        Returns
+        -------
+        list
+            una lista di tuple, ognuna delle quali contiene:
+            - il training set
+            - il test set
+            - i valori target del training set
+            - i valori target del test set
+
+        """
+        #assicura che la sequenza di numeri casuali generata sia sempre la stessa per lo stesso valore di random_state
+        np.random.seed(self.random_state)
+        #generazione degli indici
+        indices = np.random.permutation(self.data.index)
+        #generazione dei fold
+        fold_size = int(len(self.data)/self.K)
+        for i in range(self.K):
+            test_index = indices[i*fold_size:(i+1)*fold_size] #seleziona gli indici per il test set
+            train_index = indices[~np.isin(indices, test_index)]
+            #generazione dei train set e test set
+            train = self.data.loc[train_index] #loc seleziona le righe con gli indici indicati
+            test = self.data.loc[test_index]
+            #generazione dei target
+            train_target = self.target.loc[train_index]
+            test_target = self.target.loc[test_index]
+            self.fold.append((train, test, train_target, test_target))
+        return self.fold
