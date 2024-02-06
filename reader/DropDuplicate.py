bbcc7a1e36d249609d3926436ce9be719d5cf967
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-06 14:40:52 +0100

    Correzione nome classe

--- reader/DropDuplicate.py
+++ reader/DropDuplicate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # la classe drop duplicate elimina i campioni che sono presenti più volte all'interno del dataset
-class Duplicate:
+class DropDuplicate:
     #il metodo drop riceve in ingresso un dataset e restituisce il dataset senza campioni duplicati
     def drop(self,dataset):
         if dataset.duplicated().any():
