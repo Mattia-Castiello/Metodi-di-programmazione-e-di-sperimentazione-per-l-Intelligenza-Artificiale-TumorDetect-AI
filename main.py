8206eb410cc3855768b73fa4bd2aa89d4f7ed915
Gabriele Peluzzi <g.peluzzi@teleconsys.it>
2024-02-08 16:23:58 +0100

    divisione input in metodi

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from manage.Input import Input
+
+
 def main():
     input_obj = Input()
     input_obj.get_input()
-    # Add your code here to perform the desired operations based on the user input
 
 if __name__ == "__main__":
     main()
--- manage/Input.py
+++ manage/Input.py
@@ -7,7 +7,7 @@ class Input:
         self.K = None
         self.metrics = []
 
-    def get_input(self):
+    def get_k(self):
         while True:
             try:
                 self.k = int(input("Inserisci il numero di vicini da utilizzare per il classificatore: "))
@@ -15,48 +15,56 @@ class Input:
             except ValueError:
                 print("Errore: Inserisci un numero intero valido.")
 
+    def get_evaluation_method(self):
         print("Scegli come valutare il modello:")
         print("1. Holdout")
         print("2. KFold (KFoldCrossValidation)")
-        while choice != 1 and choice != 2:
+        while True:
             try:
                 choice = int(input("Inserisci il numero corrispondente alla tua scelta: "))
                 if choice == 1:
                     self.evaluation = 1
-                    while True:
-                        try:
-                            self.training = int(input("Inserisci la percentuale di dati da utilizzare nel set di training (0-100): "))
-                            if 0 < self.training < 100:
-                                break
-                            else:
-                                print("Errore: Inserisci un numero intero valido compreso tra 0 e 100.")
-                        except ValueError:
-                            print("Errore: Inserisci un numero intero valido.")
+                    self.get_training_percentage()
                     break
                 elif choice == 2:
                     self.evaluation = 2
-                    while True:
-                        try:
-                            self.K = int(input("Inserisci il numero di esperimenti K: "))
-                            if self.K > 0:
-                                break
-                            else:
-                                print("Errore: Inserisci un numero intero valido maggiore di 0.")
-                        except ValueError:
-                            print("Errore: Inserisci un numero intero valido.")
+                    self.get_K()
                     break
                 else:
                     print("Errore: Inserisci un numero valido.")
             except ValueError:
                 print("Errore: Inserisci un numero intero valido.")
 
+    def get_training_percentage(self):
+        while True:
+            try:
+                self.training = int(input("Inserisci la percentuale di dati da utilizzare nel set di training (0-100): "))
+                if 0 < self.training < 100:
+                    break
+                else:
+                    print("Errore: Inserisci un numero intero valido compreso tra 0 e 100.")
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def get_K(self):
+        while True:
+            try:
+                self.K = int(input("Inserisci il numero di esperimenti K: "))
+                if self.K > 0:
+                    break
+                else:
+                    print("Errore: Inserisci un numero intero valido maggiore di 0.")
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def get_metrics(self):
         print("Scegli quali metriche devono essere validate:")
         print("1. Accuracy Rate")
         print("2. Error Rate")
         print("3. Sensitivity")
         print("4. Specificity")
         print("5. Geometric Mean")
-        print("6. All the above")
+        print("6. Tutte le metriche disponibili")
         while True:
             choices = input("Inserisci i numeri corrispondenti alle metriche separate da uno spazio: ")
             choices = choices.split()
@@ -80,3 +88,8 @@ class Input:
             elif choice == "6":
                 self.metrics = [1, 2, 3, 4, 5]
                 break
+
+    def get_input(self):
+        self.get_k()
+        self.get_evaluation_method()
+        self.get_metrics()
