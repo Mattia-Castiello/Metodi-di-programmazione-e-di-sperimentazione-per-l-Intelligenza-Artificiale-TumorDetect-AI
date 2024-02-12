8caf4a42f22e5933c883ff4a14ffec4d7a7e9160
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-12 10:54:39 +0100

    Ora gestisce l'eccezione se gli viene passato un file path errato

--- dataprocessing/reader/Reader_dataset_factory.py
+++ dataprocessing/reader/Reader_dataset_factory.py
@@ -7,14 +7,20 @@ from .Reader_dataset_json import Reader_dataset_json
 class Reader_dataset_factory:
 
     # Il metodo "readerFactoryManager" accetta il percorso di un file come input e, in funzione dell'estensione del file,
-    # nel caso in cui il formato non sia csv o json, gestisce l'eccezione convertendo il filename in un file csv,
-    # inoltre istanzia il lettore di un file csv
+    # nel caso in cui il formato non sia csv o json, gestisce l'eccezione avvertendo di inserire un file path con una
+    # estensione supportata e dà la possibilità di inserire un nuovo filepath
+
     def readerFactoryManager(self, filepath):
         try:
             if filepath.endswith('.csv'):
                 return Reader_dataset_csv().parse(filepath)
             elif filepath.endswith('.json'):
                 return Reader_dataset_json().parse(filepath)
-        except RuntimeError:
-                print("Unrecognized file format for the dataset")
-                print("\nChange the file extension to CSV")
+            else:
+                raise RuntimeError("L'estensione del file non è tra quelle supportate .")
+        except RuntimeError as error:
+            print(error)
+            print("\nCambia l'estensione del file in csv o json")
+            print("Inserisci un nuovo filepath:")
+            new_filepath = input()
+            return self.readerFactoryManager(new_filepath)
