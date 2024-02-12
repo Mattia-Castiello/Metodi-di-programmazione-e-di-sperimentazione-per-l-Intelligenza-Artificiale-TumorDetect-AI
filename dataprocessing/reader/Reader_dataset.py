e858ac96592055026a1bb115a6ea7dd2d3dfa49f
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-12 11:25:59 +0100

    Migliorie commenti

--- dataprocessing/reader/Reader_dataset.py
+++ dataprocessing/reader/Reader_dataset.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 #Classa astratta Reader la quale dichiara il metodo parse
 class Reader_dataset(ABC):
+    """
+           Metodo astratto per analizzare un dataset e restituire i dati.
+
+           Parameters:
+           filename (str): Nome del file del dataset.
+
+           Returns:
+           Dipende dall'implementazione delle sotto classi.
+    """
     @abstractmethod
     #il metodo pass prende in ingresso il nome di un qualsiasi dataset
     def parse(self, filename):
