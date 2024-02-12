964e2c5e83f87807f236c786ede7515e04adf54d
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-12 11:27:43 +0100

    Migliorie commenti

--- dataprocessing/Bridger.py
+++ dataprocessing/Bridger.py
@@ -1,23 +1,34 @@
 import pandas as pd
 import numpy as np
 
-#La classe bridger si occupa di colmare i valori Nan all'interno del dataset
-
+# La classe Bridger si occupa di colmare i valori NaN all'interno del dataset
 class Bridger:
-    # il metodo impution va a colmare i valori nulli effettuando l'imputazione
-    # prende in ingresso il dataset e restituisce il dataset modificato
-    def impution(self, dataset):
-        if dataset.isnull().sum().any():
-            col_Nan = dataset.columns[dataset.isnull().sum() > 0].tolist()
-            # calcoliamo la mediana dei valori e la sostituiamo al posto dei valori nulli
-            # scegliamo la mediana poichè inserisce valori interi
-            #inoltre se il valore nullo appartiene alla colonna Class eliminiamo la riga
-            #in modo da non assegnare un tumore a pazienti sani e viceversa
+    # Il metodo imputation va a colmare i valori nulli effettuando l'imputazione.
+    # Prende in ingresso il dataset e restituisce il dataset modificato.
+    def imputation(self, dataset):
+        """
+        Effettua l'imputazione dei valori nulli nel dataset.
 
+        Parameters:
+        dataset (DataFrame): Il DataFrame contenente il dataset.
+
+        Returns:
+        DataFrame: Il DataFrame modificato con i valori nulli colmati.
+        """
+        # Verifichiamo se ci sono valori nulli nel dataset
+        if dataset.isnull().sum().any():
+            # Se ci sono valori nulli, otteniamo le colonne con valori nulli
+            col_Nan = dataset.columns[dataset.isnull().sum() > 0].tolist()
+
+            # Per ogni colonna con valori nulli
             for col in col_Nan:
+                # Se la colonna è 'Class', eliminiamo le righe con valori nulli
                 if col == 'Class':
                     dataset = dataset[dataset[col].notna()]
                 else:
+                    # Altrimenti, calcoliamo la mediana dei valori e la sostituiamo
+                    # al posto dei valori nulli nella colonna corrente
                     revenue_median = dataset[col].median()
                     dataset[col].fillna(revenue_median, inplace=True)
+
         return dataset
