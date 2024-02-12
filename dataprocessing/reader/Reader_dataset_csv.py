69a19523be4b5b7787498d774ea1ee519676a110
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-12 11:20:24 +0100

    Migliorie commenti

--- dataprocessing/reader/Reader_dataset_csv.py
+++ dataprocessing/reader/Reader_dataset_csv.py
@@ -1,19 +1,27 @@
 import pandas as pd
 from .Reader_dataset import Reader_dataset
-# Importiamo la classe astratta Reader_dataset
-# La classe Reader_dataset_csv si occupa di leggere un dataset con estensione csv
+# Importa la classe astratta Reader_dataset
+
+# La classe Reader_dataset_csv si occupa di leggere un dataset con estensione CSV
 class Reader_dataset_csv(Reader_dataset):
-    # Il metodo parse prende in ingresso il percorso del file e restituisce in uscita un dataset
     def parse(self, filepath):
-        # Leggiamo il file CSV  e lo salviamo in un DataFrame
+        """
+        Metodo per analizzare un file CSV e restituire un DataFrame.
+
+        Parameters:
+        filepath (str): Percorso del file CSV.
+
+        Returns:
+        pandas.DataFrame: DataFrame contenente i dati del file CSV.
+        """
+        # Legge il file CSV e lo converte in un DataFrame
         df_csv = pd.read_csv(filepath)
 
-        # Poiché il 'Sample code number' non è univoco per ogni campione, lo indicizziamo in modo univoco
-        # tramite una sequenza di numeri crescenti
+        # Poiché il 'Sample code number' non è univoco per ogni campione, lo indicizza in ordine crescente ed univoco
         df_csv['Sample code number'] = range(1, len(df_csv) + 1)
 
-        # Impostiamo l'indice del DataFrame sulla nuova colonna 'Sample code number'
+        # Imposta la colonna 'Sample code number' come indice del DataFrame
         df_csv = df_csv.set_index('Sample code number')
 
-        # Restituiamo il DataFrame con l'indice univoco
+        # Restituisce il DataFrame con l'indice univoco
         return df_csv
