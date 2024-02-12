066c8bb7065aec417fefdc755356de309f0c9570
Matiia Castiello <mattia.castiello001@gmail.com>
2024-02-12 11:18:29 +0100

    Aggiunta di commenti

--- dataprocessing/reader/Reader_dataset_json.py
+++ dataprocessing/reader/Reader_dataset_json.py
@@ -1,19 +1,26 @@
 import pandas as pd
-from .Reader_dataset import Reader_dataset
-#importiamo la classe astratta
+from .Reader_dataset import Reader_dataset  # Importa la classe astratta Reader_dataset
 
-#la classe Reader_dataset_csv si occupa di leggere un dataset con estensione json
-#la classe Reader_dataset_json si occupa di leggere un dataset con estensione json
+
+# La classe Reader_dataset_json si occupa di leggere un dataset con estensione JSON
 class Reader_dataset_json(Reader_dataset):
     def parse(self, filepath):
-        # il metodo parse prende in ingresso il percorso del file e restituisce in uscita un dataset
+        """
+        Metodo per analizzare un file JSON e restituire un DataFrame.
 
-        df_json=pd.read_json(filepath)
-        # essendo che il sample code number non è univoco per ogni campione andiamo
-        # ad indicizzare la colonna in ordine crescente ed univoca
+        Parameters:
+        filepath (str): Percorso del file JSON.
 
+        Returns:
+        pandas.DataFrame: DataFrame contenente i dati del file JSON.
+        """
+        # Legge il file JSON e lo converte in un DataFrame
+        df_json = pd.read_json(filepath)
+
+        # Essendo che il 'Sample code number' non è univoco per ogni campione, lo indicizza in ordine crescente ed univoco
         df_json['Sample code number'] = range(1, len(df_json) + 1)
 
+        # Imposta la colonna 'Sample code number' come indice del DataFrame
         df_json = df_json.set_index('Sample code number')
 
         return df_json
